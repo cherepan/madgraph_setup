8c1eefa733cbdc63f5fff1d3426f9a875823e019
Vladimir <vladimir.cherepanov@cern.ch>
2019-01-20 19:42:41 +0100

    fix qsuber

--- condor_tut/short.sh
+++ condor_tut/short.sh
@@ -4,8 +4,10 @@ printf "Start time: "; /bin/date
 printf "Job is running on node: "; /bin/hostname
 printf "Job running as user: "; /usr/bin/id
 printf "Job is running in directory: "; /bin/pwd
-echo
-printf $1,$2,$3,'\n'
+echo "$ncpu"
+echo "$nEv"
+echo "$seed"
+printf $nEv,$seed,$ncpu,'\n'
 echo "\n Working hard..."
 sleep 3
 echo "Science complete!"
